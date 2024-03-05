c18e7e69daf9c57143718705ed939b50fc1c6b1f
Alejandro Gutiérrez <cesaragg.2003@mail.com>
2024-03-05 09:40:57 -0600

    Corrección

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,31 +1,17 @@
 
-import 'package:countdown_app/presentation/Provides/Contdown_provider.dart';
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class login extends StatelessWidget {
-  const login({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final countdownProvider = Provider.of<CountDownProvider>(context);
-
-    return loginView(
-        countdownProvider: countdownProvider);
-  }
-}
-
-class loginView extends StatelessWidget {
-
-  const loginView({Key? key})
-      : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: const Text(
+          "Screen 3",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
     );
   }
 }
